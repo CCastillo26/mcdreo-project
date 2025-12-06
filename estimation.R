babdc422481c6c38b6376c03ed0fee7973cd75f9
CCastillo26 <charly.castillo2627@gmail.com>
2025-12-05 19:10:40 -0500

    Update estimation.R

--- estimation.R
+++ estimation.R
@@ -16,12 +16,38 @@ calculate_stats <- function(mcdreo_df) {
 #` Run t-tests
 #' @description Runs t-tests comparing pre, post-2020 growth
 #' @param mcdreo_df data frame
-#' @return data frame with three columns
+#' @return data frame with five columns
+run_growth <- function(mcdreo_df) {
+  region_list <- unique(mcdreo_df$region)
+  
+  # Create empty data frame
+  test_results <- data.frame(region = character(), mean_pre = numeric(),
+                             mean_post = numeric(), t_statistic = numeric(),
+                             p_value = numeric())
+  
+  for (i in region_list) {
+    region_data <- mcdreo_df[mcdreo_df$region == i, ]
+    
+    pre_growth <- region_data$gdp_growth[region_data$period == "pre2020"]
+    post_growth <- region_data$gdp_growth[region_data$period == "post2020"]
+    
+    t <- t.test(pre_growth, post_growth)
+    
+    new_row <- data.frame(region = i, mean_pre = mean(pre_growth), 
+                          mean_post = mean(post_growth), 
+                          t_statistic = as.numeric(t$statistic), 
+                          p_value = t$p.value)
+    
+    test_results <- rbind(test_results, new_row) # Append to data frame
+  }
+  
+  test_results
+}
 
-# calculate_stats
-# run_growth
-# fit_growth
-
-
-# simulate_growth
-# plot_growth
+#` Fit growth model
+#' @description Fits growth model on region, period, interaction
+#' @param mcdreo_df data frame
+#' @return linear model
+fit_growth <- function(mcdreo_df) {
+  lm(gdp_growth ~ region * period, data = mcdreo_df)
+}
