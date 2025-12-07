ed6c9d8b9d4dd99816f789b3f6d2a89e3b1ae15e
CCastillo26 <charly.castillo2627@gmail.com>
2025-12-06 21:43:36 -0500

    Update plot.R

--- plot.R
+++ plot.R
@@ -1,1 +1,19 @@
-# plot_growth
+source("data.R")
+library(tidyverse)
+
+mcdreo_df <- filter_data(mcdreo_df)
+
+#` Plot growth
+#' @description Plot simulated, IMF projected growths
+#' @param mcdreo_df data frame
+#' @param sim_df data frame
+#' @return ggplot object
+plot_growth <- function(mcdreo_df, sim_df) {
+  start_year <- 2020
+  
+  # Split IMF data
+  pre_data <- mcdreo_df[mcdreo_df$year < start_year, ]
+  post_data <- mcdreo_df[mcdreo_df$year >= start_year, ]
+  
+  
+}
